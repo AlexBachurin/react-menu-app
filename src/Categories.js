64a6317d3abcb2bde036efdd97078f01659f0137
Alex <alexbachurin91@gmail.com>
2021-09-07 20:44:18 +0300

    display categories + created somewhat unique key for them

--- src/Categories.js
+++ src/Categories.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-export default function Categories() {
+export default function Categories({ category }) {
+    //transform array from props to jsx 
+    const categoryBtns = category.map(btn => {
+        return (
+            // and set unique key since its list
+            <button key={Date.now() + Math.random()} className="filter-btn">{btn}</button>
+        )
+    })
     return (
-        <div>
-
+        <div className="btn-container">
+            {/* display category btns */}
+            {categoryBtns}
         </div>
     )
 }
